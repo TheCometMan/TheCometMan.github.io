36673e617d07412b0c5fa3e954ea18d8ef209a19
TheCometMan <arsene.kazumba24@gmail.com>
2024-08-09 00:30:55 +0100

    Update main.js

--- Sandcastle-Builder-Automation/main.js
+++ Sandcastle-Builder-Automation/main.js
@@ -6,7 +6,7 @@ BeachBall.lootBoxes = ['boosts', 'badges', 'hpt', 'ninj', 'chron', 'cyb', 'bean'
 BeachBall.resetCaged = 0;
 
 //Version Information
-BeachBall.version = '5.2.0.7';
+BeachBall.version = '5.2.1.0';
 BeachBall.SCBversion = '3.4121'; //Last SandCastle Builder version tested
 
 //BB Audio Alerts Variables
